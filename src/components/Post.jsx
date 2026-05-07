aca7238e6edf4bf35537c716eeb34d21afb9f8cf
SamarthSable <samarthsable02@gmail.com>
2026-05-07 23:17:19 +0530

    Mobile Responsive

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,7 +18,7 @@ export default function Post() {
 
   return (
     <div>
-      <div className="grid grid-cols-3 gap-2 text-center items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {post.map((p) => (
           <div
             key={p.id}
